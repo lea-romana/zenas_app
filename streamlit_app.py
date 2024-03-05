ee345199902974bb1370e1db3d56c3b35ad796d8
Lea Romana <62459614+lea-romana@users.noreply.github.com>
2024-03-05 18:35:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,47 +19,18 @@ option = st.selectbox(
     pd_df["COLOR_OR_STYLE"])
 
 temp_df = pd_df[pd_df["COLOR_OR_STYLE"] == option].reset_index(drop = True)
-st.write(temp_df.loc[0]["DIRECT_URL"])
 
 product_caption = f'Our warm, confortable, {option} sweatsuit!'
 
-# streamlit.image(
-#     df2[0],
-#     width=400,
-#     caption= product_caption
-# )
-
-st.dataframe(temp_df)
-st.stop()
-
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients',
-    my_df,
-    max_selections=5
+streamlit.image(
+    temp_df.loc[0]["DIRECT_URL"],
+    width=400,
+    caption= product_caption
 )
 
-if ingredients_list:
-    # st.text(ingredients_list)
-    ingredients_string=''
-    for fruit in ingredients_list:
-        ingredients_string+=fruit+' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit,' is ', search_on, '.')
-        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        # st.text(fruityvice_response)
-    # st.write(ingredients_string)
+st.write("Price: ", temp_df.loc[0]["PRICE"])
+st.write("Available sizes: ", temp_df.loc[0]["SIZE_LIST"])
+st.write("Bonus: ", temp_df.loc[0]["UPSELL_PRODUCT_DESC"])
 
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-    
-    # st.write(my_insert_stmt)
-    # st.stop()
-    time_to_insert = st.button('Submit Order')
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered {name_on_order}!', icon="✅")
-
-# import requests
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response)
+# st.dataframe(temp_df)
+# st.stop
